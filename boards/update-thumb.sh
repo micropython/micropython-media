9d29aa49e7d170f211f8dc54107064f4eeb65287
Jim Mussared <jim.mussared@gmail.com>
2021-10-19 22:11:28 +1100

    boards/update-thumb.sh: Slightly increase thumbnail res.
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- boards/update-thumb.sh
+++ boards/update-thumb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm */*.thumb.jpg
-for file in */*; do convert $file -thumbnail 200 ${file/.jpg/.thumb.jpg}; done
+for file in */*; do convert $file -thumbnail 300 ${file/.jpg/.thumb.jpg}; done
